5e2d90a11243a66b07928572f225d69597a7d30e
Edgar Ramirez <kurt_th@edgars-macbook-pro.local>
2018-01-17 19:39:23 -0600

    Testing Analyzing bot.

--- StayTuned/AppDelegate.swift
+++ StayTuned/AppDelegate.swift
@@ -13,9 +13,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    func returnSomeInteger() -> Int {
+        return 123;
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        returnSomeInteger()
+        
         return true
     }
 
